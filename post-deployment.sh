be2d02f2c6b81f7d43dd50e1fad47e9d87f7dfcc
shumatep <pshumate@easydynamics.com>
2020-09-16 13:58:22 -0400

    revert global change

--- post-deployment.sh
+++ post-deployment.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cf ssh -N -T -L 8081:localhost:8081 give-api-gateway &
 curl -i -X POST http://localhost:8081/services --data name='usps' --data url='https://give-ipp-usps-intelligent-hyena-tl.app.cloud.gov/' & 
-curl -X POST http://localhost:8081/plugins/ --data "name=key-auth"
 kill $!
