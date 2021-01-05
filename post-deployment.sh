7a8ea0ea1f5dd3468cb5457dc99cbe9017b87251
pshumateEDC <pshumate@easydynamics.com>
2021-01-05 17:02:07 -0500

    use correct name of test module

--- post-deployment.sh
+++ post-deployment.sh
@@ -4,5 +4,5 @@ cf ssh -N -T -L 8081:localhost:8081 give-api-gateway &
 PID=$!
 sleep 3
 curl -X POST http://localhost:8081/plugins/ --data "name=key-auth"
-pytest tests/KongTest.py
+pytest tests/test_kong.py
 kill $PID
