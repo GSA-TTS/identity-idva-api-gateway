0feef4d152aaa1339ce834364f36424473e221a5
pshumateEDC <pshumate@easydynamics.com>
2021-01-13 15:37:00 -0500

    add comments to executables

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+# Pseudo deployment executable
+# Deploys app to cloud.gov with cloud foundry 
+
+# dependency phase
 python3.9 -m pip install -r tests/requirements.txt
 
+# deployment phase
 cf push --vars-file vars.yml
 
+# post reployment phase
 ./post-deployment.sh
--- post-deployment.sh
+++ post-deployment.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-echo 'post deployment script'
+
+# Post Deployment Kong API Gateway Setup
+# Deploys plugins, services, and routes
+# Runs unit tests 
+
+# setup ssh tunnel to Kong Admin API
 cf ssh -N -T -L 8081:localhost:8081 give-api-gateway &
 PID=$!
 sleep 3
+
+# add auth service
 curl -X POST \
     --url "localhost:8081/services" \
     --data "name=auth-service" \
     --data "url=https://konghq.com/"
+
+# add routes to auth service
 curl -i -X POST \
     --url http://localhost:8081/services/auth-service/routes \
     --data 'paths[]=/auth' \
     --data 'methods[]=GET&methods[]=POST'
+
+# add oauth plugin to service
 curl -X POST http://localhost:8081/services/auth-service/plugins/ \
     --data "name=oauth2"  \
     --data "config.scopes=rpname" \
@@ -19,5 +30,7 @@ curl -X POST http://localhost:8081/services/auth-service/plugins/ \
     --data "config.enable_authorization_code=true" \
     --data "config.global_credentials=true" \
     --data "config.accept_http_if_already_terminated=true"
+
+# run unit tests
 pytest tests/test_kong.py
 kill $PID
