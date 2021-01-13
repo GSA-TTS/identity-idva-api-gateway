5e22bae6fd3eb40976f865de07f42843b2a96c07
pshumateEDC <pshumate@easydynamics.com>
2021-01-13 14:35:29 -0500

    update unit tests

--- tests/test_kong.py
+++ tests/test_kong.py
@@ -5,14 +5,32 @@ def test_get_kong_check_status_code():
     response = requests.get("http://localhost:8081")
     assert response.status_code == 200
 
-def test_get_kong_plugins_verify_hmac():
-    response = requests.get("http://localhost:8081/plugins")
+def test_get_kong_plugins_verify_oauth():
+    response = requests.get("http://localhost:8081/plugins/")
     body_dictionary = json.loads(response.text)
 
-    plugin_data = body_dictionary["data"]
+    data = body_dictionary["data"]
 
-    for x in range(len(plugin_data)):
-        if plugin_data[x]["name"] == "key-auth":
+    for x in range(len(data)):
+        plugin = data[x]
+        if plugin["name"] == "oauth2":
+            config = plugin["config"]
+            if config["enable_client_credentials"] == True and config["enable_authorization_code"] == True and config["global_credentials"] == True:
+                assert True
+                return
+            assert False
+    assert False
+
+def test_get_kong_service_verify_auth_service():
+    response = requests.get("http://localhost:8081/services/")
+    body_dictionary = json.loads(response.text)
+
+    data = body_dictionary["data"]
+
+    for x in range(len(data)):
+        service = data[x]
+        if service["name"] == "auth-service":
             assert True
             return
+
     assert False
