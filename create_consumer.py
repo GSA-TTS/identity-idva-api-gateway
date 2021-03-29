c3bb1ad54dbd19d9e10b677ef50d13c22ae2d074
shumatepf <shumatepfs@gmail.com>
2021-03-29 18:25:30 -0400

    Add python script for the generation of consumers+credentials Consumer+credential generation now possible with a single script execution.

---
+++ create_consumer.py
@@ -0,0 +1,45 @@
+import argparse, requests
+
+def check_alphanumeric(value):
+    if not value.isalnum():
+        raise argparse.ArgumentTypeError("%s is a valid alphanumeric string" % value)
+    return value
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create Kong Consumer')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-i', '--id', type=check_alphanumeric, required=True)
+    args = parser.parse_args()
+    
+    user_data = {
+        "username": args.username,
+        "custom_id": args.id
+    }
+    url = "http://localhost:8081/consumers/"
+    oauth_data = {
+        "name": "oauth2"
+    }
+
+    # create consumer
+    r = requests.post(url, data = user_data)
+    if (r.status_code != 201):
+        raise SystemError("Error creating consumer.")
+    print("Kong consumer successfully created.")
+
+    # generate credentials
+    r = requests.post(url + args.username + "/oauth2", data = oauth_data)
+    if (r.status_code != 201):
+        print("Error creating credentials. Deleting consumer...")
+        # try to delete consumer
+        r = requests.delete(url + args.username)
+        if (r.status_code != 204):
+            raise SystemError("Error deleting consumer. Admin invervention required.")
+        raise SystemError("Error creating credentials. Consumer deleted.")
+    
+    # consumer creation successful
+    # credential distribution
+    json_response = r.json()
+    print("GIVE credentials generatated, please store in safe location:")
+    print("client_id: ", json_response["client_id"])
+    print("client_secret: ", json_response["client_secret"])
