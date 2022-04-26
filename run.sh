a491bea0a4e1824dcf120ed19d9670569de9c733
folksgl <Gfolks14@gmail.com>
2022-04-26 15:34:13 -0400

    Refactor Kong env vars into separate script

--- run.sh
+++ run.sh
@@ -1,23 +1,10 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
-# Make location of libs configurable
-LOCAL='/home/vcap/deps/0/apt/usr/local'
-
-export LD_LIBRARY_PATH=$LOCAL/lib:$LOCAL/lib/lua/5.1/:$LOCAL/openresty/luajit/lib:$LOCAL/openresty/pcre/lib:$LOCAL/openresty/openssl111/lib:$LD_LIBRARY_PATH
-export LUA_PATH="$LOCAL/share/lua/5.1/?.lua;$LOCAL/share/lua/5.1/?/init.lua;$LOCAL/openresty/lualib/?.lua"
-export LUA_CPATH="$LOCAL/lib/lua/5.1/?.so;$LOCAL/openresty/lualib/?.so"
-export PATH=$LOCAL/bin/:$LOCAL/openresty/nginx/sbin:$LOCAL/openresty/bin:$PATH
-
-# Ensure references to /usr/local resolve correctly
-grep -irIl '/usr/local' ../deps/0/apt | xargs sed -i -e "s|/usr/local|$LOCAL|"
-
-export KONG_LUA_PACKAGE_PATH=$LUA_PATH
-export KONG_LUA_PACKAGE_CPATH=$LUA_CPATH
+source ./set_kong_env.sh
 
 # Generate the kong.yaml state file
-/home/vcap/deps/0/apt/usr/bin/envsubst < kong-config.yaml > /home/vcap/app/kong.yaml
-/home/vcap/deps/0/apt/usr/bin/envsubst < kong.conf.template > /home/vcap/app/kong.conf
+envsubst < kong-config.yaml > ~/kong.yaml
+envsubst < kong.conf.template > ~/kong.conf
 
 instance_identity_cert_folder=$(dirname "$CF_INSTANCE_CERT")
 
