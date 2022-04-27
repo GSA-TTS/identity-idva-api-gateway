8bf674b50093086e85a8818d232ef5c7ba4ad8f2
folksgl <Gfolks14@gmail.com>
2022-04-27 09:54:44 -0400

    Add sidecar process for reloading kong when certs are updated

--- run.sh
+++ run.sh
@@ -6,17 +6,5 @@ source ./set_kong_env.sh
 envsubst < kong-config.yaml > ~/kong.yaml
 envsubst < kong.conf.template > ~/kong.conf
 
-instance_identity_cert_folder=$(dirname "$CF_INSTANCE_CERT")
-
-# An infinite-loop function that will watch the cf instance identity certs for changes
-# and tell kong to reload its configuration if the files are updated.
-instance_identity_cert_watcher() {
-  while inotifywait -q -e modify "$instance_identity_cert_folder" ; do 
-    kong reload -c ./kong.conf --v
-  done
-}
-
-instance_identity_cert_watcher &
-
 # Start the main Kong application.
 kong start -c ./kong.conf --v
---
+++ watch-certs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+source ./set_kong_env.sh
+kong version # Sanity check to ensure 'kong' is on the current $PATH
+
+instance_identity_cert_folder=$(dirname "$CF_INSTANCE_CERT")
+
+# Infinite-loop that will watch the cf instance identity certs for changes
+# and tell kong to reload its configuration if the files are updated.
+while inotifywait -q -e modify "$instance_identity_cert_folder" ; do 
+  kong reload -c ./kong.conf --v
+done
