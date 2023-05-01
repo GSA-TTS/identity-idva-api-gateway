b90c1894d072b38faae194a3ed659e6a2dc51428
dzaslavskiy <dzaslavskiy@users.noreply.github.com>
2023-05-01 00:19:46 -0400

    Update kong version
    Update to cflinuxfs4
    update kong repo and version
    fix for apt buildpack version change
    fix for library path changes

--- common/set_kong_env.sh
+++ common/set_kong_env.sh
@@ -3,8 +3,8 @@
 # Make location of libs configurable
 LOCAL='/home/vcap/deps/0/apt/usr/local'
 
-export LD_LIBRARY_PATH=$LOCAL/lib:$LOCAL/lib/lua/5.1/:$LOCAL/openresty/luajit/lib:$LOCAL/openresty/pcre/lib:$LOCAL/openresty/openssl111/lib:$LD_LIBRARY_PATH
-export LUA_PATH="$LOCAL/share/lua/5.1/?.lua;$LOCAL/share/lua/5.1/?/init.lua;$LOCAL/openresty/lualib/?.lua"
+export LD_LIBRARY_PATH=$LOCAL/lib:$LOCAL/lib/lua/5.1/:$LOCAL/openresty/luajit/lib:$LOCAL/openresty/pcre/lib:$LOCAL/openresty/openssl111/lib:$LOCAL/kong/lib:$LD_LIBRARY_PATH
+export LUA_PATH="$LOCAL/share/lua/5.1/?.lua;$LOCAL/share/lua/5.1/?/init.lua;$LOCAL/openresty/lualib/?.lua;$LOCAL/openresty/lualib/?/init.lua"
 export LUA_CPATH="$LOCAL/lib/lua/5.1/?.so;$LOCAL/openresty/lualib/?.so"
 export PATH=$LOCAL/bin/:$LOCAL/openresty/nginx/sbin:$LOCAL/openresty/bin:$PATH
 
